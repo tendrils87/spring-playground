5a5c71751489fb7f028b0af9283cee667c591c65
Sean <sean.ryan.martin@gmail.com>
2020-12-10 11:37:44 -0500

    JPA

---
+++ src/main/java/com/example/demo/LessonRepository.java
@@ -0,0 +1,28 @@
+package com.example.demo;
+
+import org.springframework.data.repository.CrudRepository;
+import org.springframework.web.bind.annotation.*;
+
+public interface LessonRepository extends CrudRepository<Lesson, Long> {
+    @RestController
+    @RequestMapping("/lessons")
+    public class LessonsController {
+
+        private final LessonRepository repository;
+
+        public LessonsController(LessonRepository repository) {
+            this.repository = repository;
+        }
+
+        @GetMapping("")
+        public Iterable<Lesson> all() {
+            return this.repository.findAll();
+        }
+
+        @PostMapping("")
+        public Lesson create(@RequestBody Lesson lesson) {
+            return this.repository.save(lesson);
+        }
+
+    }
+}
